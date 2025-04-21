b2cea9fad30c858d2007682564d10cd9ed6b079f
IbrahemZakria <https://github.com/IbrahemZakria>
2025-04-21 18:06:11 +0200

    add ADDnote function

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -1,8 +1,23 @@
+import 'dart:math';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
-  AddNoteCubit() : super(AddNoteInitial());
+  AddNoteCubit() : super(AddNoteInitialState());
+
+  void addnotes(NoteModel note) {
+    try {
+      emit(AddNoteLoadingState());
+      var notesbox = Hive.box<NoteModel>('knotebox');
+      notesbox.add(note);
+      emit(AddNoteSuccessState());
+    } catch (e) {
+      emit(AddNoteErrorState(e.toString()));
+    }
+  }
 }
