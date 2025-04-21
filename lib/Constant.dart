dbe3853036a92fedf93b6d51fd9d69cda1a4a576
IbrahemZakria <https://github.com/IbrahemZakria>
2025-04-21 15:51:53 +0200

    init hive

--- lib/Constant.dart
+++ lib/Constant.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color kprimaryColor = Color(0xff62fcd7);
 const Color ksecodaryColor = Color.fromARGB(255, 142, 171, 198);
-String kcollectionmessage = 'messages';
+String knotebox = 'notesbox';
 String KbaseUrl = 'https://fakestoreapi.com';
